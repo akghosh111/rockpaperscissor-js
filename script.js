00d8e049a7fbd08f761f752ffdfa60e154eed105
akghosh111 <ghoshkiran2017@gmail.com>
2024-03-29 21:00:51 +0530

    computer selection & playerselection added

---
+++ script.js
@@ -0,0 +1,46 @@
+console.log("hello fucking world")
+
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    const choice = options[Math.floor(Math.random() * options.length)];
+    console.log(choice);
+    return choice;
+
+}
+
+function checkWinner(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie";
+    }
+    else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ) {
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    const result = checkWinner(playerSelection, computerSelection)
+    if (result == "Tie") {
+        return "Its a Tie!"
+    }
+    else if (result == "Player") {
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    }
+    else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+console.log(playRound(playerSelection, computerSelection));
